3c97c85f9fffecf61ebe4949d45ea63352fb75e0
akram <akramkhalid12@gmail.com>
2019-03-18 19:26:32 -0500

    styling implementation

--- examples/cra/src/App.js
+++ examples/cra/src/App.js
@@ -3,7 +3,21 @@ import { Happy } from "react-happytext";
 
 class App extends Component {
   render() {
-    return <Happy value="akram" />;
+    return (
+      <>
+        <span
+          style={{
+            fontSize: "200px",
+            fontWeight: "700",
+            fontFamily: "Avenir",
+            letterSpacing: "-0.4rem",
+            display: "block"
+          }}
+        >
+          <Happy value="HappyText" />
+        </span>
+      </>
+    );
   }
 }
 
